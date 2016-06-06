b73fc0dbc6f4b2e0046b01a26aa48d500693ab31
CarlisleChan <duziyixiangsi@gmail.com>
2016-06-06 16:20:02 +0800

    Init project , add seeker test

---
+++ app/src/main/java/com/carlisle/incubators/Seekbar/SeekBarActivity.java
@@ -0,0 +1,18 @@
+package com.carlisle.incubators.Seekbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.carlisle.incubators.R;
+
+/**
+ * Created by chengxin on 6/3/16.
+ */
+
+public class SeekBarActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seekbar);
+    }
+}
